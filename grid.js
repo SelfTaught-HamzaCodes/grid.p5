0f71d6140f4112b36d4d82553648cd2928df8bcf
Muhammad Hamza Saeed <123310424+SelfTaught-HamzaCodes@users.noreply.github.com>
2023-10-31 14:29:57 +0500

    Update grid.js

--- grid.js
+++ grid.js
@@ -2,32 +2,44 @@
  * Draws a grid on the canvas with the specified spacing and customizable colors.
  *
  * @param {number} spacing - The distance between grid lines and labels.
+ * @param {boolean} [pointer=false] - To enable/disable coordinates at the pointer's current position.
  * @param {string} [gridLineColor='black'] - Color for the grid lines.
  * @param {string} [labelColor='grey'] - Color for the labels.
  * @param {string} [pointerColor='red'] - Color for the pointer text.
  */
-function drawGrid(spacing, gridLineColor = 'black', labelColor = 'grey', pointerColor = 'red') {
+function drawGrid(spacing=50, pointer=false, gridLineColor = 'black', labelColor = 'grey', pointerColor = 'red') {
   push(); // Save the current drawing settings.
 
     /// Grid Generation ///
-    stroke(gridLineColor);
-    fill(labelColor);
-    strokeWeight(1);
-
     const generation = max(width, height);
 
     for (let pos = 0; pos < generation; pos += spacing) {
-      line(pos, 0, pos, height);
-      line(0, pos, width, pos);
+      push();
+        strokeWeight(1);
+        stroke(gridLineColor);
+        line(pos, 0, pos, height);
+        line(0, pos, width, pos);
+      pop();
+      
       textAlign(CENTER, CENTER);
-      text(pos, 10, pos);
-      text(pos, pos, 10);
+      
+      push();
+          fill(labelColor)
+          noStroke();
+          text(pos, 10, pos);
+          text(pos, pos, 10);
+        pop();
     }
 
     /// Coordinates at pointer position ///
-    fill(pointerColor);
-    noStroke();
-    text(`${mouseX},${mouseY}`, mouseX, mouseY);
+    if (pointer)
+    {
+
+      fill(pointerColor);
+      noStroke();
+      text(`${mouseX},${mouseY}`, mouseX, mouseY);
+    }
+
 
   pop(); // Restore the previous drawing settings.
 }
